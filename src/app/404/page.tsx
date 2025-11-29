61b4f53c82893778fb9afc52cd2cc7b928e3d7ea
Flávio R. Troszczanczuk <flavioczuk@gmail.com>
2025-11-29 20:50:17 -0300

    error 404

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,59 +1,41 @@
 // libraries
-import clsx from 'clsx'
 import Link from 'next/link'
 
 // components
 import MagneticButton from '@/components/Utils/Animations/MagneticButton'
 
-// images
-import banner from '@/assets/img/banners/lab.jpg'
-import headquarter from '@/assets/img/banners/headquarters.jpg'
-
 // utils
 import { pages } from '@/utils/routes'
 
 export const metadata = {
-	title: 'Error 404 | Agência Esfera'
+	title: 'Erro 404: Página não encontrada | Agência Esfera'
 }
 
 export default function Error404() {
 	return (
 		<main>
+			<section className='bg-black text-white text-center'>
+				<div className='base-container min-h-svh flex flex-col items-center justify-center py-35 md:py-40 xl:py-50 gap-10 sm:gap-15'>
 
-			Oppz! Error 404
+					<h1 className='text-100 font-heading font-semibold uppercase tracking-tighter mt-10 sm:mt-20 max-sm:text-[25vw]!'>
+						Erro <span className='text-yellow'>404</span>
+					</h1>
 
-			<section className=''>
-				<div className='container'>
-					<div className='row'>
-						
-						<div className='col-md-6'>
+					<p className='text-20'>
+						Parece que o link que você seguiu não está mais disponível.
+					</p>
 
-							<h2 className=''>
-								Opa! Esta página está sentindo mal.
-							</h2>
+					<MagneticButton>
+						<Link
+							href={pages.home}
+							className='button button--hollow-white'
+						>
+							Voltar para a Home
+						</Link>
+					</MagneticButton>
 
-							<p>
-								<b>Parece que o link que você seguiu não está em nossa fórmula.</b> Mas não se preocupe. Temos a receita certa para você voltar ao caminho certo! <br /><br />
-
-								Ou sinta-se à vontade para <Link href={pages.contact} className='hover-underline'><b>entrar em contato</b></Link>.
-							</p>
-
-							<MagneticButton>
-								<Link href={pages.home} className='button button--black'>
-									Voltar para a Home
-								</Link>
-							</MagneticButton>
-
-						</div>
-
-						<div className='col-md-6'>
-							
-						</div>
-
-					</div>
 				</div>
 			</section>
-
 		</main>
 	)
 }
