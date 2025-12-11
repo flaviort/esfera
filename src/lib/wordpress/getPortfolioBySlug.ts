8ee4b8a34c37329f0f887dfd7235dc61ffa15cff
Flávio R. Troszczanczuk <flavioczuk@gmail.com>
2025-12-11 13:44:12 -0300

    wp integration pt3

--- src/lib/wordpress/getPortfolioBySlug.ts
+++ src/lib/wordpress/getPortfolioBySlug.ts
@@ -2,7 +2,58 @@
 import { GraphQLClient, gql } from 'graphql-request'
 
 // types
-import { PortfolioItem } from './getPortfolio'
+export type PortfolioImage = {
+    node: {
+        srcSet: string
+        sizes: string
+        mediaItemUrl: string
+    }
+}
+
+export type PortfolioCategory = {
+    nodes: {
+        name: string
+    }[]
+}
+
+export type PortfolioItem = {
+    id: string
+    title: string
+    slug: string
+    date: string
+    portfolioFields: {
+        area: string
+        client: string
+        excerpt: string
+        subtitle: string
+        year: string
+        thumbnail: PortfolioImage
+        bgImage: PortfolioImage
+        mainContent?: Array<{
+            fieldGroupName?: string
+            
+            multitext_content?: string
+            multitext_title?: string
+            multitext_subtitle?: string
+            
+            fsmedia_alt?: string
+            fsmedia_image?: {
+                node: {
+                    srcSet: string
+                    sizes: string
+                    mediaItemUrl: string
+                }
+            }
+            fsmedia_video?: {
+                node: {
+                    mediaItemUrl: string
+                }
+            }
+
+        }>
+    }
+    portfolioCategories: PortfolioCategory
+}
 
 // constants
 const endpoint = process.env.WP_GRAPHQL as string
@@ -34,6 +85,30 @@ const query = gql`
                     node {
                         sizes
                         mediaItemUrl
+                    }
+                }
+                mainContent {
+                    ... on PortfolioFieldsMainContentMultitextLayout {
+                        fieldGroupName
+                        multitext_title
+                        multitext_subtitle
+                        multitext_content
+                    }
+                    ... on PortfolioFieldsMainContentFsmediaLayout {
+                        fieldGroupName
+                        fsmedia_alt
+                        fsmedia_image {
+                            node {
+                                srcSet
+                                sizes
+                                mediaItemUrl
+                            }
+                        }
+                        fsmedia_video {
+                            node {
+                                mediaItemUrl
+                            }
+                        }
                     }
                 }
             }
